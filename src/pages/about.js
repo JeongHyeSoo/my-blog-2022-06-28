a8b67f87bd93a8933e530d32c44768e77deb217c
JeongHyeSoo <nailonly@naver.com>
2022-06-29 07:07:07 +0900

    이미지 import 수정

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,7 @@ import Layout from "../components/Layout";
 function AboutPage() {
     return (
         <Layout pageTitle="About Me">
-            <p>자기소개자기소개자기소개자기소개자기소개자기소개자기소개
-            자기소개자기소개자기소개자기소개자기소개자기소개자기소개
+            <p>Hello my name is JHS
             </p>
         </Layout>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>Home PageHome PageHome PageHome PageHome PageHome PageHome PageHome PageHome Page
-        Home PageHome PageHome PageHome PageHome PageHome PageHome Page</p>
+      <p>My Little Home Page</p>
     <StaticImage alt=""
     src="https://images.mypetlife.co.kr/content/uploads/2021/10/19151330/corgi-g1a1774f95_1280.jpg"
     />
